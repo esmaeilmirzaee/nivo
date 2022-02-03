e49c4470fe63a9c2e1e695e42ab9061e361f2e7b
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-02-03 08:00:28 +0330

    Front: Initalize application

--- core/src/app.ts
+++ core/src/app.ts
@@ -1,12 +1,20 @@
 import cors from 'cors';
 import express from 'express';
+import client from './utils/db';
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 let PORT = '5000';
-
-app.listen(PORT, () => {
-    console.log(`App is listening on ${PORT}`);
-});
+try {
+    client.connect().then(() => {
+        console.log(`✅ Connected to database...`);
+        app.listen(PORT, () => {
+            console.log(`App is listening on ${PORT}`);
+        });
+    });
+} catch (err: any) {
+    console.log(`🔥 Unable to connect to database...`);
+    console.error(err.message);
+}
